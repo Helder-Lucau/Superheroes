8e6214ebd544bd40d1cafbf1d1702305c619a773
Helder-Lucau <helderlucau@gmail.com>
2023-10-01 16:42:36 +0300

    creating relationship between tables

--- server/models.py
+++ server/models.py
@@ -16,6 +16,9 @@ class Hero(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
 
+    # Relationship one to many 
+    hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
+
 class Power(db.Model, SerializerMixin):
     __tablename__='powers'
 
@@ -26,6 +29,9 @@ class Power(db.Model, SerializerMixin):
     description = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
+
+    # Relationship one to many
+    hero_powers = db.relationship('HeroPower', back_populates='power', cascade='all, delete-orphan')
 
 class HeroPower(db.Model, SerializerMixin):
     __tablename__='hero_powers'
@@ -39,4 +45,8 @@ class HeroPower(db.Model, SerializerMixin):
 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # Relationship patterns many to one
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
 
