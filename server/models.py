d6bd2ec7279a36b44af3912cb3c93250324def6a
Helder-Lucau <helderlucau@gmail.com>
2023-10-01 15:08:55 +0300

    create table powers

--- server/models.py
+++ server/models.py
@@ -14,4 +14,11 @@ class Hero(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
 
-# add any models you may need. 
+class Power(db.Model):
+    __tablename__='powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
