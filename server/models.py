7e98ea3ebc17502653ea73b6481a87e6fdfdeb13
Helder-Lucau <helderlucau@gmail.com>
2023-10-01 15:42:32 +0300

    created table hero powers

--- server/models.py
+++ server/models.py
@@ -22,3 +22,13 @@ class Power(db.Model):
     description = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
+
+class HeroPower(db.Model):
+    __tablename__='hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
