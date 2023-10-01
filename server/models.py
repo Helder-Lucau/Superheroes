662cb843efee3f2c03c55d2c410b477b16d7dc60
Helder-Lucau <helderlucau@gmail.com>
2023-10-01 10:56:29 +0300

    create table heroes

--- server/models.py
+++ server/models.py
@@ -2,11 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 
+# Initialize database
 db = SQLAlchemy()
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
 
 # add any models you may need. 
