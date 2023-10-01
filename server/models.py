9fcc86b09364d479a9b1fadc02fe16ceb2526669
Helder-Lucau <helderlucau@gmail.com>
2023-10-01 16:28:29 +0300

    serialization

--- server/models.py
+++ server/models.py
@@ -5,8 +5,10 @@ from sqlalchemy.orm import validates
 # Initialize database
 db = SQLAlchemy()
 
-class Hero(db.Model):
+class Hero(db.Model, SerializerMixin):
     __tablename__ = 'heroes'
+
+    serialize_rules = ('-hero_powers.hero',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -14,8 +16,10 @@ class Hero(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
 
-class Power(db.Model):
+class Power(db.Model, SerializerMixin):
     __tablename__='powers'
+
+    serialize_rules = ('-hero_powers.power',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -23,12 +27,16 @@ class Power(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
 
-class HeroPower(db.Model):
+class HeroPower(db.Model, SerializerMixin):
     __tablename__='hero_powers'
+
+    serialize_rules = ('-hero.hero_powers', '-power.hero_powers',)
 
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
